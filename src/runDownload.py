622065a57de48be6450051053823200cfc78098f
Vitor Peres <vitor.peres@hygiabank.com>
2022-10-04 10:33:54 -0300

    teste

--- src/runDownload.py
+++ src/runDownload.py
@@ -1,6 +1,13 @@
 
 from pytube import YouTube
 import os
+import cv2, pafy
+import numpy as np 
+import os 
+import math 
+import csv
+from moviepy.editor import *
+import datetime
 
 def downloadYouTube(videourl, path):
 
@@ -10,4 +17,52 @@ def downloadYouTube(videourl, path):
         os.makedirs(path)
     yt.download(path)
 
-downloadYouTube('https://youtu.be/esfChb49_Rk', 'data/video/')
+
+def downloadFrames():
+
+    entries = os.listdir('./data/video/')
+
+    for ent in entries:
+
+        vidcap      = cv2.VideoCapture('./data/video/' + ent)
+        
+        clip        = VideoFileClip('./data/video/' + ent) 
+        duration    = clip.duration    
+
+        video_time = str(datetime.timedelta(seconds = int(duration)))
+
+        n_frames    = int(vidcap.get(cv2.CAP_PROP_FRAME_COUNT))
+        fps         = int(vidcap.get(cv2.CAP_PROP_FPS))
+
+        try:
+            if not os.path.exists('./data/images/' + ent):
+                os.makedirs('./data/images/' + ent)
+        except OSError:
+            print('Error: Creating directory of data')
+
+
+        currentFrame = 0
+        while(True):
+            
+            # Capture frame-by-frame
+            ret, frame = vidcap.read()
+
+            if not ret:
+                break
+
+            # Saves image of the current frame in jpg file
+            name = './data/images/' + ent + '/frame_' + str(currentFrame) + '.jpg'
+            # print ('Creating...' + name)
+            cv2.imwrite(name, frame)
+
+            # To stop duplicate images
+            currentFrame += 1
+
+        vidcap.release()
+        cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+
+    # downloadYouTube('https://youtu.be/esfChb49_Rk', 'data/video/')
+
+    downloadFrames()
---
+++ src/runOpenFace.py
@@ -0,0 +1,34 @@
+
+from unicodedata import name
+import numpy as np 
+import os 
+from os import path
+import subprocess
+import shutil
+
+def createFileVideo():
+
+    entries = os.listdir('./data/video/')
+
+    for ent in entries:
+
+        try:
+            if not os.path.exists('./output/'):
+                os.makedirs('./output/')
+        except OSError:
+            print('Error: Creating directory of data')
+
+        exec1 = path + 'data\\video\\' + ent
+        exec2 = path + 'output\\'
+
+        comando = ' -f ' + '"' +  exec1 + '"'
+
+        os.system('"C:\\OpenFace\\FeatureExtraction.exe' + comando + ' -out_dir ' + exec2 + '"')
+
+
+if __name__ == '__main__':
+
+    # mudar path
+    path = "D:\\PythonProjects\\faces-analytics\\"
+
+    createFileVideo()
